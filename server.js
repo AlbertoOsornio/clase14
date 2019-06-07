539e39770e97c2759a2abce4a7f9c7e4f53115c4
Rafa Osornio <osornio.rafael@gmail.com>
2019-06-06 20:42:47 -0500

    subiendo routings

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let response = ""
 
-app.get('/', function (req, res) {
-res.send('Hello World!')
-})
+app.get('/',handlerGET)
+app.post('/',handlerGET)
+//app.update('/',handlerGET)
+//app.delete('/',handlerGET)
+
+
 
 app.listen(port, function () { 
 console.log(`Example app listening on port ${port}!`)
 })
+
+
+function handlerGET(req,res){
+    res.send('Contestando una peticion por metodo GET')
+}
+
+
+function handlerPOST(req,res){
+    response.send('Contestando una peticion por metodo POST')
+}
+
+
+function handlerUPDATE(req,res){
+    response.send('Contestando una peticion por metodo UPDATE')
+}
+
+function handlerDelete(req,res){
+    response.send('Contestando una peticion por metodo DELETE')
+}
